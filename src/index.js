a0ff292086968133c58d0efaa6beb198cfabc5b7
Serhii Prokopchuk <gosusosed@gmail.com>
2021-05-31 21:15:50 +0300

    published

---
+++ src/index.js
@@ -0,0 +1,65 @@
+import './sass/main.scss';
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.targetDate = targetDate.getTime();
+    this.timer = document.querySelector(selector);
+    this.days = this.timer.querySelector('[data-value="days"]');
+    this.hours = this.timer.querySelector('[data-value="hours"]');
+    this.mins = this.timer.querySelector('[data-value="mins"]');
+    this.secs = this.timer.querySelector('[data-value="secs"]');
+    this.start();
+  }
+
+  start() {
+    if (this.getTime() <= 0) {
+      this.days.textContent = 0;
+      this.hours.textContent = 0;
+      this.mins.textContent = 0;
+      this.secs.textContent = 0;
+      return;
+    } else {
+      this.intervalId = setInterval(this.updateTimer, 1000);
+    }
+  }
+
+  getTime() {
+    const timeNow = Date.now();
+    const time = this.targetDate - timeNow;
+    return time;
+  }
+
+  countTime() {
+    const time = this.getTime();
+    const days = Math.floor(time / (1000 * 60 * 60 * 24))
+      .toString()
+      .padStart(2, '0');
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      .toString()
+      .padStart(2, '0');
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))
+      .toString()
+      .padStart(2, '0');
+    const secs = Math.floor((time % (1000 * 60)) / 1000)
+      .toString()
+      .padStart(2, '0');
+    return { hours, days, mins, secs };
+  }
+
+  updateTimer = () => {
+    if (this.getTime() <= 0) {
+      clearInterval(this.intervalId);
+      return;
+    }
+
+    this.days.textContent = this.countTime().days;
+    this.hours.textContent = this.countTime().hours;
+    this.mins.textContent = this.countTime().mins;
+    this.secs.textContent = this.countTime().secs;
+  };
+}
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Dec 31, 2021'),
+});
